34ce601a90fe597587defa8c6c41a402401ad9c6
ammar <akidwai03@gmail.com>
2023-10-07 14:04:32 +0530

    h

---
+++ automate boring stuff w python/chapter 1 - intro/flow control/testingForLoop.py
@@ -0,0 +1,11 @@
+# for loop with a start and an end value
+
+# for i in range(10,20):
+#     print(i)
+
+# defined a starting point (10) and an end point (20) for the loop
+
+for i in range(10, 20, 2):
+    print(i)
+
+# increments i in steps of 2
