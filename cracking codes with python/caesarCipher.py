994a293ee8c03bd6481860bb1c4db9302e708104
ammar <akidwai03@gmail.com>
2023-10-13 21:42:41 +0530

    e

---
+++ cracking codes with python/caesarCipher.py
@@ -0,0 +1,9 @@
+# code for caesar cipher in python
+
+mode = input('enter a mode\n1 : encrypt a message\n2 : decrypt a message')
+
+input = input('Enter a message : ')
+
+key = input('Enter the encryption/decryption key : ')
+
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'      # all caps variables are treated as constants
